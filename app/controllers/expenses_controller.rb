0b1d67d7d563073fb5fdcc134efad75c4e9f8f74
Naseef03 <muhammednaseef03@gmail.com>
2024-11-01 20:22:12 +0530

    Set up basic structure for adding new expenses to the list

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -4,11 +4,24 @@ class ExpensesController < ApplicationController
   end
 
   def new
+    @expense = Expense.new
   end
 
   def create
+    @expense = Expense.new(expense_params)
+    if @expense.save
+      redirect_to expenses_path, notice: "Expense added successfully"
+    else
+      render :new
+    end
   end
 
   def show
+  end
+
+  private
+
+  def expense_params
+    params.require(:expense).permit(:amount, :description, :category, :date)
   end
 end
