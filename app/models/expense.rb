7098059ffd08cceff01d42de70a42753bc8660d9
Naseef03 <muhammednaseef03@gmail.com>
2024-12-22 23:20:59 +0530

    Adds user authentication

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,7 @@
 class Expense < ApplicationRecord
+  validates :space, presence: true
+  validates :date, presence: true
+  validates :category, presence: true
+  validates :account, presence: true
+  validates :amount, comparison: { greater_than: 0 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get "expenses/index"
   get "expenses/create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
